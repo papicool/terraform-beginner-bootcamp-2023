d4cd0b11ff291ae21a04fe0f85cba7da18bd2d4f
Papa Moussa FALL <papemfall@gmail.com>
2023-09-26 14:38:57 +0000

    create S3 bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+
+provider "aws" {
+  # Configuration options
+}
+
+
+resource "random_string" "bucket_name" {
+  length           = 32
+  lower = true
+  upper = false
+  special          = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
