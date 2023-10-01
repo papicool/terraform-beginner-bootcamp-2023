d1b28f11e0c742af9806f3326fac22857d70bdf5
papicool <papemfall@gmail.com>
2023-10-01 12:47:51 +0200

    structure terraform module

--- main.tf
+++ main.tf
@@ -1,46 +1,5 @@
-terraform {
-
-#  backend "remote" {
-#    hostname = "app.terraform.io"
-#    organization = "MyTerraformBootcamp"
-#
-#    workspaces {
-#      name = "terra-house-1"
-#    }
-#  }
 
 
-   cloud {
-     organization = "MyTerraformBootcamp"
- 
-     workspaces {
-       name = "terra-house-1"
-     }
-   }
-
-
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
-}
 
 
 resource "random_string" "bucket_name" {
@@ -52,10 +11,8 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
