09b9770fe4d34743b7fb48f2ea1042459faed129
papicool <papemfall@gmail.com>
2023-10-02 19:35:30 +0200

    wip - Content Delivery Network

--- modules/terrahouse_aws/resource_storage.tf
+++ modules/terrahouse_aws/resource_storage.tf
@@ -25,6 +25,7 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   source = var.index_html_file
+  content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -37,6 +38,7 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   source = var.error_html_file
+  content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
