30edbc4e175d01436a4fa245bc715e3b9db06926
papicool <papemfall@gmail.com>
2023-10-03 16:24:19 +0200

    wip - Invalidate Cache and Local Exec

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "website_endpoint" {
   description = "website_endpoint of our static website"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
